224d55a5acc63fb845005b2521a345ec92b46f66
shahid <shahidthaha2005@gmail.com>
2024-10-22 20:02:36 +0530

    new commit

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 def fun1(req,a):
     print("hello")
     a=[10,11,12,13]
@@ -8,3 +8,60 @@ def fun1(req,a):
     return HttpResponse(a)
 def fun2(req):
     return HttpResponse("habibi")
+def task(req,salary,year):
+    if year>5:
+        bonus=(salary/100*5)
+        return HttpResponse(bonus)
+    else:
+        return HttpResponse('year of service lessthan 5')
+def task1(req,city):
+    if city=='Delhi':
+        return HttpResponse('Red fort')
+    elif city=='Agra':
+        return HttpResponse('Taj mahal')
+    elif city=='Jaipur':
+        return HttpResponse('Jal mahal')
+    else:
+        return HttpResponse('invalid input')
+def task2(req,num):
+    n=num%10
+    if n%3==0:
+        return HttpResponse('divisible by 3')
+    else:
+        return HttpResponse('not divisible by 3')
+    
+def task3(req,day):
+    if day==1:
+        return HttpResponse('sunday')
+    elif day==2:
+        return HttpResponse('monday')
+    elif day==3:
+        return HttpResponse('tuesday')
+    elif day==4:
+        return HttpResponse('wednesday')
+    elif day==5:
+        return HttpResponse('thursday')
+    elif day==6:
+        return HttpResponse('friday')
+    elif day==7:
+        return HttpResponse('saturday')
+    else:
+        return HttpResponse('invalid')
+
+def task4(req,cost):
+    if cost>100000:
+        return HttpResponse(cost*0.15)
+    elif cost>50000 and cost<=100000:
+        return HttpResponse(cost*0.10)
+    else:
+        return HttpResponse(cost*0.05)
+
+def task5(req,unit):
+    if unit<=100:
+        return HttpResponse(unit*0)
+    elif unit>100 and unit<=200:
+        return HttpResponse((unit-100)*5)
+    else:
+        return HttpResponse((unit-200)*10+500)
+
+        # Create your views here.
--- project/project/urls.py
+++ project/project/urls.py
@@ -21,5 +21,11 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('demo/',views.fun1),
-    path('fun2/ <a>',views.fun2)
+    path('fun2/ <a>',views.fun2),
+    path('task/<int:salary>/<int:year>',views.task),
+    path('task1/<city>',views.task1),
+    path('task2/<int:num>',views.task2),
+    path('task3/<day>',views.task3),
+    path('task4/<int:cost>',views.task4),
+    path('task5/<int:unit>',views.task5),
 ]
